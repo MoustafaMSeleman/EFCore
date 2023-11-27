23ed969a58016f4a468cbd55f9c861f53d9e838b
MoustafaMSeleman <moustafaseleman784@gmail.com>
2023-11-27 14:20:45 +0200

    Create a console app in a sln, install 3 packages Microsoft.Data.SqlClient Microsoft.extensions.configuration Microsoft.extensions.configuration.json, add appsettings.json file with the string connection and finally try to access the value of the connection string

---
+++ EFCore/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder().AddJsonFile("D:\\DotNet Core\\MyProjects\\EFCore\\EFCore\\appsettings.json").Build();
+//Console.WriteLine(config.GetSection("DefaultConnection").Value);
+
+
